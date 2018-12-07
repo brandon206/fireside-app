c8dd513243488933876b13ecd4c081eb2ddb78bd
Brandon Park <brandonpark206@gmail.com>
2018-12-07 11:47:01 -0800

    got firebase collection to show up

--- src/reducers/chat_reducer.js
+++ src/reducers/chat_reducer.js
@@ -1,12 +1,13 @@
-import types from '../actions';
-import { bindActionCreators } from 'redux';
+import types from '../actions/types';
 
 const DEFAULT_STATE = {
-
+    messages: {}
 };
 
 export default (state = DEFAULT_STATE, action) => {
-    switch(action.types){
+    switch(action.type){
+        case types.GET_CHAT_MESSAGES:
+            return {...state, messages: action.messages}
         default:
             return state;
     }
